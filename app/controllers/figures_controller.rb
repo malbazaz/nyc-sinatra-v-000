b24f61e63c9b46cdf69f88a0a0e3b231aef786a1
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-01 13:53:21 +0000

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -10,9 +10,24 @@ class FiguresController < ApplicationController
   end
 
   post '/figures' do
-    #binding.pry
+  #  binding.pry
+    @figure = Figure.create(name: params[:Name])
+    redirect to "/figures/#{@figure.id}"
   end
 
+  get '/figures/:id' do
 
+  erb :'figures/show'
+  end
+
+  get '/figures/:id/edit' do
+
+  erb :'figures/edit'
+  end
+
+  post '/figures/:id' do
+
+  redirect to "/figures/#{@figure.id}"
+  end
 
 end
