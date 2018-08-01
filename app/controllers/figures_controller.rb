727d3e33cdf69241634ef4fcb73548afd7c9b6d5
Mo Al <malbazaz@mba2018.hbs.edu>
2018-08-01 02:20:51 +0000

    Done.

---
+++ app/controllers/figures_controller.rb
@@ -0,0 +1,18 @@
+require 'pry'
+class FiguresController < ApplicationController
+
+get '/figures' do
+erb :'figures/index'
+end
+
+get '/figures/new' do
+erb :'figures/new'
+end
+
+post '/figures' do
+binding.pry
+end
+
+
+
+end
